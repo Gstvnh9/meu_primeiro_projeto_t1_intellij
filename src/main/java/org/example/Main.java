ca5be8a8a31d02e429fd6be822e762fa97bb9e3e
GUSTAVO COSTA LIMA <gustavo.lima223@senaisp.edu.br>
2026-08-28 14:34:08 -0300

    feat: Add my first java project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        short numeroPequeno = 1;
+        int numeroInteiro = numeroPequeno;
+        byte numeroPequenino = (byte) numeroPequeno;
+
+        System.out.println("O número final é: " + numeroPequenino);
+    }
+}
