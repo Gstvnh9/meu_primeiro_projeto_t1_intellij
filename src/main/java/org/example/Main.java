1d32bff3291d819855d0f933ddc0e4d829e6f880
Gustavinho <gustavocostalima9@outlook.com>
2026-09-01 13:59:42 -0300

    feat: Add Questão 1 - Verificação de Maioridade

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,18 +8,17 @@ import java.util.Locale;
 public class Main {
      static void main() {
 
-         // Precisa de um acesso do Interpasse
-
          System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
          Scanner entrada = new Scanner(System.in);
          entrada.useLocale(Locale.US);
 
-         System.out.println("Indique uma quantidade, em segundos:");
-         int segundos = entrada.nextInt();
+         System.out.println("Indique a sua idade:");
+         int idade = entrada.nextInt();
 
-         int minutos = segundos / 60;
-         int horas = minutos / 60;
-
-         System.out.println(segundos + " segundos representam: " + horas + " horas, " + minutos + " minutos e " + segundos + " segundos." );
+         if (idade >= 18) {
+             System.out.println("Maior de idade");
+         } else {
+             System.out.println("Menor de idade");
+         }
      }
 }
