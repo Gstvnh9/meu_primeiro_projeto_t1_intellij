ececff978d0763b42701e0e61d61129beed10bcc
53188493851 <gustavocostalima9@outlook.com>
2026-09-04 14:40:31 -0300

    feat: Add Questão 9 - Auxílio para Curso de Idiomas

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,15 +12,29 @@ public class Main {
          Scanner entrada = new Scanner(System.in);
          entrada.useLocale(Locale.US);
 
-         System.out.println("Você utiliza veículo próprio para trabalhar?");
-         System.out.println("Responda com 'SIM' ou 'NÃO':");
-         String resposta = entrada.nextLine();
+         System.out.println("============================");
+         System.out.println("      Lista de cargos:      ");
+         System.out.println("============================");
+         System.out.println("       Admnistrativos:      ");
+         System.out.println("- Assistente");
+         System.out.println("============================");
+         System.out.println("         Liderança          ");
+         System.out.println("- Gerente");
+         System.out.println("============================");
+         System.out.println("        Operacional         ");
+         System.out.println("- Engenheiro");
+         System.out.println("============================");
+         System.out.println("Nos indique o seu cargo na empresa:");
+         String cargo = entrada.nextLine();
 
-         if(resposta.equalsIgnoreCase("SIM")){
-             System.out.println("Você pode solicitar auxilio combustível!");
+         if (cargo.equalsIgnoreCase ("ASSISTENTE")){
+             System.out.println("Você possui direito ao curso de Idiomas!");
+         } else if (cargo.equalsIgnoreCase ("GERENTE")){
+             System.out.println("Você possui direito ao curso de Idiomas!");
+         } else if (cargo.equalsIgnoreCase ("ENGENHEIRO")) {
+             System.out.println("Você não possui direito ao curso de Idiomas!");
          } else {
-             System.out.println("Você não possui direito ao benefício!");
+             System.out.println("Esse cargo não existe em nossa empresa.");
          }
-
      }
 }
