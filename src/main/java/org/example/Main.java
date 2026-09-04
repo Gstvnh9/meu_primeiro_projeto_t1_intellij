1eaa1f40e75f78846c1661844ff4eed448824181
53188493851 <gustavocostalima9@outlook.com>
2026-09-04 14:46:29 -0300

    feat: Add Questão 10 - Aprovação de Empréstimo Pessoal

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,29 +12,18 @@ public class Main {
          Scanner entrada = new Scanner(System.in);
          entrada.useLocale(Locale.US);
 
-         System.out.println("============================");
-         System.out.println("      Lista de cargos:      ");
-         System.out.println("============================");
-         System.out.println("       Admnistrativos:      ");
-         System.out.println("- Assistente");
-         System.out.println("============================");
-         System.out.println("         Liderança          ");
-         System.out.println("- Gerente");
-         System.out.println("============================");
-         System.out.println("        Operacional         ");
-         System.out.println("- Engenheiro");
-         System.out.println("============================");
-         System.out.println("Nos indique o seu cargo na empresa:");
-         String cargo = entrada.nextLine();
+         System.out.println("Informe o valor do seu salário:");
+         double salario = entrada.nextDouble();
 
-         if (cargo.equalsIgnoreCase ("ASSISTENTE")){
-             System.out.println("Você possui direito ao curso de Idiomas!");
-         } else if (cargo.equalsIgnoreCase ("GERENTE")){
-             System.out.println("Você possui direito ao curso de Idiomas!");
-         } else if (cargo.equalsIgnoreCase ("ENGENHEIRO")) {
-             System.out.println("Você não possui direito ao curso de Idiomas!");
+         System.out.println("Informe o valor desejado para empréstimo:");
+         double emprestimo = entrada.nextDouble();
+
+         double aprovacao = salario * 0.3;
+
+         if (emprestimo <= aprovacao) {
+             System.out.println("O seu empréstimo foi aprovado!");
          } else {
-             System.out.println("Esse cargo não existe em nossa empresa.");
+             System.out.println("Infelizmente você ultrapassou os limites para aprovação!");
          }
      }
 }
