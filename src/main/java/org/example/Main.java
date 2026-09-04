af9d7dd7acd04a5b6408ef8e29719ee26cb3a870
53188493851 <gustavocostalima9@outlook.com>
2026-09-04 14:01:51 -0300

    feat: Add Questão 7 - Plano de Saúde Empresarial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,13 +12,13 @@ public class Main {
          Scanner entrada = new Scanner(System.in);
          entrada.useLocale(Locale.US);
 
-         System.out.println("Informe o valor do seu salário:");
-         double salario = entrada.nextDouble();
+         System.out.println("Indique o seu tempo na empresa, em meses:");
+         int meses = entrada.nextInt();
 
-         if (salario <= 4000) {
-             System.out.println("Parabéns, você ainda possui o direito ao Vale-Refeição!");
+         if (meses > 3) {
+             System.out.println("Parabéns! Você possui direito de adesão ao benefício!");
          } else {
-             System.out.println("Infelizmente o seu salário ultrapassou o limite para adesão do benefício!");
+             System.out.println("Infelizmente você ainda não está há tempo suficiente na empresa para aderir ao benefício!");
          }
      }
 }
