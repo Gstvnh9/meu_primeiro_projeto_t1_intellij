60d76c6bbf3ae70a4677e752fcfa7214a8ce1654
53188493851 <gustavocostalima9@outlook.com>
2026-09-10 16:18:40 -0300

    feat: Add Questão 13 - Detecção de Transação Suspeita

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,16 +12,15 @@ public class Main {
          Scanner entrada = new Scanner(System.in);
          entrada.useLocale(Locale.US);
 
-         System.out.println("Indique a renda mensal do cliente:");
-         double renda = entrada.nextDouble();
+         System.out.println("Indique o valor da transação:");
+         double transacao = entrada.nextDouble();
 
-         System.out.println("Agora, indique o Score de crédito de um cliente:");
-         int score = entrada.nextInt();
-
-         if (renda > 8000 && score > 700) {
-             System.out.println("Parabéns, O seu Cartão PREMIUM foi aprovado!");
+         if (transacao > 10000) {
+             System.out.println("Esta transação foi análisada pela central:");
+             System.out.println("Considerações: Transação Suspeita. Recomendamos checar sua conta do banco!");
          } else {
-             System.out.println("Infelizmente, você não atende aos requisitos necessários para aprovação do Cartão PREMIUM.");
+             System.out.println("Esta transação foi análisada pela central:");
+             System.out.println("Considerações: Transação Comum.");
          }
      }
 }
