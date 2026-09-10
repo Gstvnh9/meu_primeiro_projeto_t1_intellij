49115a4485e89488ed4c2245e00a3abe873df4aa
53188493851 <gustavocostalima9@outlook.com>
2026-09-10 16:02:59 -0300

    feat: Add Questão 12 - Elegibilidade para Cartão Premium

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -12,19 +12,16 @@ public class Main {
          Scanner entrada = new Scanner(System.in);
          entrada.useLocale(Locale.US);
 
-         System.out.println("Indique o valor investido pelo cliente:");
-         double investimento = entrada.nextDouble();
+         System.out.println("Indique a renda mensal do cliente:");
+         double renda = entrada.nextDouble();
 
-         if (investimento <= 10000) {
-             System.out.println("Você foi classificado como um cliente BRONZE!");
-         } else if (investimento <= 50000) {
-             System.out.println("Você foi classificado como um cliente PRATA!");
-         } else if (investimento <= 100000) {
-             System.out.println("Você foi classificado como um cliente OURO!");
-         } else if (investimento > 100000) {
-             System.out.println("Você foi classificado como um cliente PLATINUM!");
+         System.out.println("Agora, indique o Score de crédito de um cliente:");
+         int score = entrada.nextInt();
+
+         if (renda > 8000 && score > 700) {
+             System.out.println("Parabéns, O seu Cartão PREMIUM foi aprovado!");
          } else {
-             System.out.println("É impossível investir esse valor!");
+             System.out.println("Infelizmente, você não atende aos requisitos necessários para aprovação do Cartão PREMIUM.");
          }
      }
 }
